267d9b07f7d12b22142ce26da28d318afd816f37
Rafael Marin <100976428+Rafaelmarinr@users.noreply.github.com>
2023-07-06 12:15:57 -0400

    Update chatgptservice.py

--- chatgptservice.py
+++ chatgptservice.py
@@ -2,7 +2,7 @@ import openai
 
 def GetResponse(text):
     try:
-        openai.api_key = "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"
+        openai.api_key = "#token"
         result = openai.Completion.create(model = "text-davinci-003", prompt = text, n = 1, max_tokens = 500)
 
         response = result.choices[0].text
